036c57f2e6eebd2772bd894fa4a79fa111ebe550
Jhonnatan Vazquez <jhonykingmusic@hotmail.com>
2024-12-02 12:45:52 -0600

    Adding tailwind css

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 uppercase text-stone-200 p-4 sm:px-6 md:text-base text-sm flex items-center justify-between">
+      <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500 flex items-center justify-around uppercase px-4 py-3 border-b-8 border-stone-200 sm:px-6">
       {" "}
-      <Link to="/">Fast React Pizza Co.</Link>
+      <Link to="/" className="tracking-widest ">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
