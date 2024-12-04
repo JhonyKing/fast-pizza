eeef20e7371daf8e8eee730bf4e7dfa662eb53db
Jhonnatan Vazquez <jhonykingmusic@hotmail.com>
2024-12-04 16:52:50 -0600

    Adding and fiing tailwind

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,3 +1,10 @@
-export default function Loader() {
-  return <div className="loader"></div>;
+
+function Loader() {
+  return (
+  <div className="absolute bg-slate-200/50 backdrop-blur-sm flex items-center inset-0 justify-center">
+
+  <div className="loader"></div>
+  </div>
+  );
 }
+export default Loader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+export default{
+  content: ['./index.html','./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    fontFamily:{
+      sans:['Roboto Mono, monospace'],
+    },
+    extend: {
+      height:{
+        screen:'100dvh',
+      },
+      fontFamily:{
+        sans:['Roboto Mono, monospace'],
+      },
+    },
   },
   plugins: [],
 };
